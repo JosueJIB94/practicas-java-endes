e39a482e67b4780a96064f9f894599007b299d49
JosueJIB94 <145494209+JosueJIB94@users.noreply.github.com>
2023-11-28 20:30:48 +0100

    Add files via upload

---
+++ Java/TablaMultiplicarServlet.java
@@ -0,0 +1,48 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class TablaMultiplicarServlet
+ */
+public class TablaMultiplicarServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TablaMultiplicarServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		int numeroTabla = Integer.parseInt(request.getParameter("numeroTabla"));
+		PrintWriter out = response.getWriter();
+		
+		for (int i=0; i<=10; i++) {
+		out.println(numeroTabla + "x" + i + " = " + (numeroTabla*i) + " ");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
